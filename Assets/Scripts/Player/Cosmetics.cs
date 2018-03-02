8b00ec5029da504f7ec0c2079f6840d0aa1a9a1c
Sebastian <seebzei@gmail.com>
2018-03-02 22:45:03 +0100

    Add random chosen skin tones and hair colors

---
+++ Assets/Scripts/Player/Cosmetics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cosmetics : MonoBehaviour 
+{
+
+	public Material[] HairColors;
+	public Material[] SkinTone;
+
+	public GameObject Head;
+	public GameObject Body;
+	public GameObject Hair;
+
+	void Start () 
+	{
+		Hair.GetComponent<Renderer>().material = HairColors[Random.Range(0, HairColors.Length)];
+		
+		Material skinTone =  SkinTone[Random.Range(0, SkinTone.Length)];
+		Head.GetComponent<Renderer>().material = skinTone;
+		Body.GetComponent<Renderer>().material = skinTone;
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
