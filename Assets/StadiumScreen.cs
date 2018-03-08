a66a926a9dcc47b2399e5c667cd4b64e910e92ee
Seebzei <seebzei@gmail.com>
2018-03-08 15:32:12 +0100

    Add big screen

---
+++ Assets/StadiumScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StadiumScreen : MonoBehaviour {
+
+	public GameObject Display;
+	public Texture camTopTexture;
+	public Texture camBlueTexture;
+	public Texture camRedTexture;
+	private GameObject _ball;
+	
+	private Material _displayMaterial;
+	public int SectorDistance{get; private set;}
+	void Start ()
+	{
+		_ball = GameObject.FindGameObjectWithTag("Ball");
+		SectorDistance = 10;
+		_displayMaterial = Display.GetComponent<Renderer>().material;
+	}
+	
+	void Update ()
+	{
+		// Change to cam based on balls position
+		if(_ball.transform.position.x > SectorDistance)
+		{
+			_displayMaterial.SetTexture("_MainTex", camRedTexture);
+		}else if(_ball.transform.position.x < -SectorDistance)
+		{
+			_displayMaterial.SetTexture("_MainTex", camBlueTexture);
+		}else
+		{
+			_displayMaterial.SetTexture("_MainTex", camTopTexture);
+		}
+	}
+}
